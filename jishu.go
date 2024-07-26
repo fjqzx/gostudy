21da0c98ffb4e5b18f69b2a2b0ae91dc73df5938
zhangxin <2298174828@qq.com>
2024-07-26 17:45:55 +0800

    查询代码的代码

--- jishu.go
+++ jishu.go
@@ -8,16 +8,18 @@ import (
 )
 
 func main() {
-	file, err := os.Open("hh.go")
+	file, err := os.Open("hh.go") /* 打开所要查询的文件 */
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file) /* 创建Scanner */
 
-	count := 0
+	count := 0 /* 计数器 */
+	
+	//遍历
 	for scanner.Scan() {
-		log.Println(scanner.Text())
+		//log.Println(scanner.Text()) /* 打印每行的消息 */
 		count++
 	}
 
