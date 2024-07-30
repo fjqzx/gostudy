50ab1403f205731d07c02baa117c4f70271168d0
zhangxin <2298174828@qq.com>
2024-07-30 15:39:58 +0800

    查询代码行数(2024.7.30)

--- linecounter.go
+++ linecounter.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 var c = make(chan int)
 var b = 0
+var wg sync.WaitGroup
 
 func walkDir(dir string) error {
 	// 使用filepath.Walk遍历目录
@@ -18,20 +20,21 @@ func walkDir(dir string) error {
 			return err // 如果遍历出错，返回错误
 		}
 
-		// 判断是否是目录
-		if info.IsDir() {
-			//fmt.Println("目录:", path)
-		} else {
-			//文件
-			fmt.Println("文件:", path)
-			go Cc()
-			Bb(path)
+		// 判断文件是否是.go文件
+		if !info.IsDir() && filepath.Ext(path) == ".go" {
+			wg.Add(1)
+			go func(path string) {
+				defer wg.Done()
+				count := Bb(path)
+				c <- count
+			}(path)
 		}
+
 		return nil // 如果没有错误，返回nil
 	})
 }
 
-func Bb(path string) {
+func Bb(path string) int {
 	file, err := os.Open(path) /* 打开所要查询的文件 */
 	if err != nil {
 		log.Fatal(err)
@@ -46,22 +49,40 @@ func Bb(path string) {
 		//log.Println(scanner.Text()) /* 打印每行的消息 */
 		count++
 	}
-	c <- count
-	fmt.Println("一共有", count, "行")
+
+	return count
 }
 
 func Cc() {
-	n := <-c
-	b = b + n
+	for count := range c {
+		b += count
+	}
 }
 
 func main() {
 	// 指定要遍历的目录
-	dir := "D:\\zxmxcx2\\ATM\\src\\images"
+	if len(os.Args) < 2 {
+		fmt.Println("请提供要遍历的目录作为命令行参数")
+		return
+	}
+
+	dir := os.Args[1]
+
+	bb := make(chan bool)
+	go func() {
+		Cc()
+		bb <- true
+	}()
 
 	// 调用walkDir函数
 	if err := walkDir(dir); err != nil {
 		fmt.Println("遍历目录时出错:", err)
 	}
+
+	wg.Wait()
+	close(c)
+
+	<-bb
+
 	fmt.Println("所有代码行数加起来有：", b)
 }
